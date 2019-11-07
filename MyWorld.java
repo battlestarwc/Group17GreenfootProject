5203457d880d4300e9959fe2e67ddfb4bc0fc465
Sophia Yuan <sjy0006@auburn.edu>
2019-11-07 17:27:06 -0600

    add background

--- MyWorld.java
+++ MyWorld.java
@@ -61,7 +61,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 600, 1, false);
-        
+        GreenfootImage background = getBackground();
+        background.setColor(Color.BLACK); 
+        background.fill();
         //Test scrolling
         addObject(new Player(), 300,300); 
         addObject(new Cannon(),200,500);
