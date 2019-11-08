174eef9a902b7b996a58a3193d40e083361d60de
battlestarsc <empholmi@gmail.com>
2019-11-07 18:45:12 -0600

    Debugged rock

--- Rock.java
+++ Rock.java
@@ -9,22 +9,34 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Rock extends Actor
 {
     private int life=15; 
-    public static final nt damage=3;
+    public static final int damage=3;
+    private boolean targetSet = false;
     private GreenfootImage rock = new GreenfootImage("Rock.png");
     /**
      * The rock will hurt the player if the player gets hit 
      * 
      */
-
+	
 	public Rock() {
-	Actor target = (Actor) (getWorld().getObjects(Player.class).get(0));
-        turnTowards(target.getX(), target.getY());	
+	//Actor target = (Actor) (getWorld().getObjects(Player.class).get(0));
+        //turnTowards(target.getX(), target.getY());	
 	}
     public void act() 
     {
        //Actor target = (Actor) (getWorld().getObjects(Player.class).get(0)); 
         //turnTowards(target.getX(), target.getY());
-	move(5);
+	if(targetSet == false) {
+		try {
+			Actor target = (Actor) (getWorld().getObjects(Player.class).get(0));
+        		turnTowards(target.getX(), target.getY());
+			targetSet = true;
+		} catch (Exception e) {
+			targetSet = False;
+		}
+	}
+	if(targetSet == true) {
+		move(5);
+	}
         checkBoundaries();
     }    
     /**
