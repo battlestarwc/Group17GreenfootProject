3bb16b9cc4f40d70b06f13ee0f53cf5aa92c2d41
Sophia Yuan <sjy0006@auburn.edu>
2019-11-17 17:04:31 -0600

    maze

--- MazeGeneratorInterface.java
+++ MazeGeneratorInterface.java
@@ -3,11 +3,11 @@
 
 
 
-/**
+
 import greenfoot.*; 
 import java.util.*;
 
-/ * Write a description of class MazeGeneratorInterface here.
+/* Write a description of class MazeGeneratorInterface here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -33,11 +33,27 @@ public class MazeGeneratorInterface
      * @param  y   a sample parameter for a method
      * @return     the sum of x and y 
      */
-    private void loadWall()
+    private void loadWall() throws RuntimeException
     {
         while (this.itr.hasNext())
         {
-            Cell c = this.itr.next();
+            Object o = this.itr.next();
+            if (!(o instanceof Cell)) {
+                throw new RuntimeException("Unable to trust iterator");
+            }
+            Cell c = (Cell) o;
+            
+            int x = c.getX();
+            int y = c.getY();
+            
+            //Now map the maze X and Y values to greenfoot X and Y values
+            //Center is 127,127 for maze and 300,300 for greenfoot
+            //Lets assume that a Wall is 50 by 50 
+            //So maze 126,126 would map to 250,250 for greenfoot
+            
+            
+            this.thisWorld.addObject(new Wall(), x, y);
+
         }
         
     }
