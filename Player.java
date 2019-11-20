9b4e3ebe9d2c7f43bfb99841ffebe283ccbbe701
Sophia Yuan <sjy0006@auburn.edu>
2019-11-20 15:45:18 -0600

    modify image

--- Player.java
+++ Player.java
@@ -19,7 +19,7 @@ public class Player extends Actor
     
     public Player() {
 
-        speed = 15;
+        speed = 10;
         this.health = 30;
         GreenfootImage playerBeautifulFace = new GreenfootImage("images/playerPic1.png");
         playerBeautifulFace.scale(40,50); //squash
